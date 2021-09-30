f1633310616bf8939bedbec73b92ec108a888b01
edowon0623@gmail.com <Dwlee$09>
2021-09-30 13:10:20 +0900

    first commit

---
+++ src/main/java/com/example/devops_demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.devops_demo;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to Simple Ecommerce ver 1.0";
+    }
+
+}
